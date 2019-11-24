3fbb77ea993bdef0fefb720db8fe41c6de4c288d
basharw <basharwattar@gmail.com>
2019-11-24 15:01:39 +0100

    Test Directory initilaize

---
+++ Test/sample/GameLoopTest.java
@@ -0,0 +1,48 @@
+package sample;
+
+import javafx.animation.AnimationTimer;
+import javafx.animation.FadeTransition;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import org.junit.Test;
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class GameLoopTest {
+    static File splashFile = new File("src/media/splash.mp4");
+    @Test
+    public void restartIngamemusic() {
+    }
+
+    @Test
+    public void restartGameovermusic() {
+    }
+
+    @Test
+    public void playEatsound() {
+    }
+
+    @Test
+    public void playDeathsound() {
+    }
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void start() {
+
+        AnimationTimer timer;
+
+        //check if file exist
+        assertEquals(splashFile.exists(),true);
+
+    }
+}
