c116805ec83802974bf5f763ea53007f5a79109e
basharw <basharwattar@gmail.com>
2019-12-08 23:28:31 +0100

    GameLoopFirst Test with Try cathch 	#	modified:   .idea/workspace.xml

--- src/test/java/asd/arch/GameLoopTest.java
+++ src/test/java/asd/arch/GameLoopTest.java
@@ -1,5 +1,7 @@
 package asd.arch;
 
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,5 +38,21 @@ class GameLoopTest {
 
     @Test
     void start() {
+    }
+
+    @Test
+    void setBackground(){
+        try {
+            Image imgSource = new Image("./media/image/grassTile.png");
+            BackgroundImage backgroundImage = new BackgroundImage(imgSource, BackgroundRepeat.REPEAT.REPEAT, BackgroundRepeat.REPEAT.REPEAT,
+                    BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+            Background backgroundView = new Background(backgroundImage);
+            //backgroundPane.setBackground(backgroundView);
+
+            assertTrue(true);
+        }
+        catch (Exception ex){
+            assertTrue(false);
+        }
     }
 }
